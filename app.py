6722c93007da694cf5f366ae5ab8ee77d11bdaee
Matt McGee <mlmcgeenc@gmail.com>
2022-02-03 18:50:08 -0500

    add experiment function

--- app.py
+++ app.py
@@ -26,17 +26,37 @@ class Hat:
 
 
 
-#def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  ballsList = []
+  for key in expected_balls.keys():
+      while expected_balls[key] > 0:
+        ballsList.append(key)
+        expected_balls[key] -= 1
+  print("The balls we're looking for are:", ballsList)
+  
+  drawnBalls = hat.draw(num_balls_drawn)
+  print("Balls drawn from the hat were:", drawnBalls)
+
+  successCount = 0
+  for ball in ballsList:
+    if ball in drawnBalls:
+      print('Found the {foundBall} ball!'.format(foundBall=ball))
+      drawnBalls.remove(ball)
+      successCount += 1
+  
+  if successCount == len(ballsList):
+    return "All balls matched!"
+  else:
+    return "Not a match."
 
 
 #random.seed(95)
 hat = Hat(blue=4, red=2, green=6)
-print(hat.contents)
-print(hat.draw(4))
-#probability = experiment(
-#    hat=hat,
-#    expected_balls={"blue": 2,
-#                    "red": 1},
-#    num_balls_drawn=4,
-#    num_experiments=3000)
-#print("Probability:", probability)
+probability = experiment(
+    hat=hat,
+    expected_balls={"blue": 2,
+                    "red": 1},
+    num_balls_drawn=4,
+    num_experiments=3000)
+#print("Hat = ", hat.contents)
+print("Probability:", probability)
